dcacf5857409b626e906032e55666f5f9aef7de7
Alex Pearson <alex@alexpear.com>
2020-01-23 13:26:04 -0500

    Remove validating webhook as test

--- src/api/snowflake.rs
+++ src/api/snowflake.rs
@@ -1,24 +1,19 @@
 //! Snowflake types that do not follow the Object<P, U> kube standard
 #![allow(non_snake_case, non_camel_case_types)]
 
-use std::collections::BTreeMap;
-use core::marker::PhantomData;
+use crate::api::{Api, KubeObject, ObjectMeta, RawApi};
 use crate::client::APIClient;
-use crate::api::{
-    RawApi, Api, KubeObject,
-    ObjectMeta
-};
+use core::marker::PhantomData;
+use std::collections::BTreeMap;
 
-
+use k8s_openapi::api::core::v1::{EventSeries, EventSource, ObjectReference};
 use k8s_openapi::apimachinery::pkg::apis::meta::v1::{MicroTime, Time};
-use k8s_openapi::api::core::v1::{EventSeries, ObjectReference, EventSource};
 
 /// Janky Event object
 ///
 /// https://kubernetes.io/docs/reference/federation/v1/definitions/#_v1_event
 #[derive(Deserialize, Serialize, Clone)]
 pub struct v1Event {
-
     pub metadata: ObjectMeta,
 
     // Require properties
@@ -31,7 +26,6 @@ pub struct v1Event {
     pub reportingInstance: String,
 
     // Properties that always seem present but arent required:
-
     #[serde(default)]
     pub message: String,
 
@@ -44,7 +38,6 @@ pub struct v1Event {
     #[serde(default, rename = "type")]
     pub type_: String,
 
-
     // Mist optionals gunk from openapi
     pub action: Option<String>,
     pub eventTime: Option<MicroTime>,
@@ -53,12 +46,13 @@ pub struct v1Event {
     pub related: Option<ObjectReference>,
     pub series: Option<EventSeries>,
     pub source: Option<EventSource>,
-
 }
 
 // Special case implementation so we can make Informer<Event> etc.
 impl KubeObject for v1Event {
-    fn meta(&self) -> &ObjectMeta { &self.metadata }
+    fn meta(&self) -> &ObjectMeta {
+        &self.metadata
+    }
 }
 
 impl Api<v1Event> {
@@ -89,10 +83,11 @@ pub struct v1Secret {
     pub type_: Option<String>,
 }
 
-
 // Special case implementation so we can make Informer<v1Secret> etc.
 impl KubeObject for v1Secret {
-    fn meta(&self) -> &ObjectMeta { &self.metadata }
+    fn meta(&self) -> &ObjectMeta {
+        &self.metadata
+    }
 }
 
 impl Api<v1Secret> {
@@ -114,11 +109,13 @@ pub struct v1ConfigMap {
     pub binaryData: BTreeMap<String, ByteString>,
 
     #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
-    pub data: BTreeMap<String, String>
+    pub data: BTreeMap<String, String>,
 }
 
 impl KubeObject for v1ConfigMap {
-    fn meta(&self) -> &ObjectMeta { &self.metadata }
+    fn meta(&self) -> &ObjectMeta {
+        &self.metadata
+    }
 }
 
 impl Api<v1ConfigMap> {
@@ -131,26 +128,26 @@ impl Api<v1ConfigMap> {
     }
 }
 
-use k8s_openapi::api::admissionregistration::v1beta1::ValidatingWebhook;
+// use k8s_openapi::api::admissionregistration::v1beta1::ValidatingWebhook;
 
-/// ValidatingWebhookConfiguration object
-#[derive(Deserialize, Serialize, Clone)]
-pub struct v1beta1ValidatingWebhookConfiguration {
-    pub metadata: ObjectMeta,
+// /// ValidatingWebhookConfiguration object
+// #[derive(Deserialize, Serialize, Clone)]
+// pub struct v1beta1ValidatingWebhookConfiguration {
+//     pub metadata: ObjectMeta,
 
-    pub webhooks: Vec<ValidatingWebhook>
-}
+//     pub webhooks: Vec<ValidatingWebhook>
+// }
 
-impl KubeObject for v1beta1ValidatingWebhookConfiguration {
-    fn meta(&self) -> &ObjectMeta { &self.metadata }
-}
+// impl KubeObject for v1beta1ValidatingWebhookConfiguration {
+//     fn meta(&self) -> &ObjectMeta { &self.metadata }
+// }
 
-impl Api<v1beta1ValidatingWebhookConfiguration> {
-    pub fn v1beta1ValidatingWebhookConfiguration(client: APIClient) -> Self {
-        Api {
-            api: RawApi::v1beta1ValidatingWebhookConfiguration(),
-            client,
-            phantom: PhantomData,
-        }
-    }
-}
+// impl Api<v1beta1ValidatingWebhookConfiguration> {
+//     pub fn v1beta1ValidatingWebhookConfiguration(client: APIClient) -> Self {
+//         Api {
+//             api: RawApi::v1beta1ValidatingWebhookConfiguration(),
+//             client,
+//             phantom: PhantomData,
+//         }
+//     }
+// }
